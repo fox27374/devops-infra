46824b42ca936af2be432e01476cbafb5ad2b447
Daniel Kofler <fox27374@gmail.com>
2024-12-20 10:05:56 +0100

    Added lb target group for every ec2 instance

--- terraform/loadbalancer.tf
+++ terraform/loadbalancer.tf
@@ -23,6 +23,8 @@ resource "aws_lb_listener" "devops-infra" {
     type             = "forward"
     target_group_arn = aws_lb_target_group.guacamole.arn
   }
+
+  depends_on = [ aws_acm_certificate_validation.lab_validation ]
 }
 
 resource "aws_lb_target_group" "guacamole" {
@@ -41,33 +43,28 @@ resource "aws_lb_target_group" "guacamole" {
   vpc_id      = aws_vpc.devops-infra.id
 }
 
-resource "aws_lb_target_group" "nginx" {
-  health_check {
-    interval            = 10
-    path                = "/"
-    protocol            = "HTTP"
-    timeout             = 5
-    healthy_threshold   = 5
-    unhealthy_threshold = 2
-  }
-  name        = "nginx"
-  port        = 8080
-  protocol    = "HTTP"
-  target_type = "instance"
-  vpc_id      = aws_vpc.devops-infra.id
-}
-
 resource "aws_lb_target_group_attachment" "guacamole" {
   target_group_arn = aws_lb_target_group.guacamole.arn
   target_id        = aws_instance.bastion.id
   port             = 80
 }
 
-resource "aws_lb_target_group_attachment" "nginx" {
-  target_group_arn = aws_lb_target_group.nginx.arn
-  target_id        = aws_instance.bastion.id
-  port             = 8080
+resource "aws_lb_target_group" "lab_tg" {
+  count        = var.EC2["lab_count"]
+  name         = aws_instance.lab[count.index].tags["Name"] # Dynamically fetch the Name tag
+  port         = 80
+  protocol     = "HTTP"
+  vpc_id      = aws_vpc.devops-infra.id
+  target_type  = "instance"
 }
+
+resource "aws_lb_target_group_attachment" "lab_attachment" {
+  count             = var.EC2["lab_count"]
+  target_group_arn  = aws_lb_target_group.lab_tg[count.index].arn
+  target_id         = aws_instance.lab[count.index].id
+  port              = 80
+}
+
 
 resource "aws_lb_listener_rule" "guacamole_rule" {
   listener_arn = aws_lb_listener.devops-infra.arn
@@ -88,21 +85,23 @@ resource "aws_lb_listener_rule" "guacamole_rule" {
   }
 }
 
-resource "aws_lb_listener_rule" "nginx_rule" {
-  listener_arn = aws_lb_listener.devops-infra.arn
+resource "aws_lb_listener_rule" "lab_listener_rule" {
+  count          = var.EC2["lab_count"]
+  listener_arn   = aws_lb_listener.devops-infra.arn
+  priority       = count.index + 2 # Ensure unique priority for each rule
 
   condition {
     host_header {
-      values = [var.NW["lb_dns_fqdn"]]
+      values = ["${aws_instance.lab[count.index].tags["Name"]}.${var.NW["domain_name"]}"]
     }
   }
 
   action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.nginx.arn
+    target_group_arn = aws_lb_target_group.lab_tg[count.index].arn
   }
 
   tags = {
-    Name = "nginx"
+    Name = "${aws_instance.lab[count.index].tags["Name"]}"
   }
 }
