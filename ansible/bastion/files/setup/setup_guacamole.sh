632672ae8ed1c6056ff289e18e2584de0d013530
Daniel Kofler <fox27374@gmail.com>
2025-01-07 18:48:07 +0100

    removed logging

--- ansible/bastion/files/setup/setup_guacamole.sh
+++ ansible/bastion/files/setup/setup_guacamole.sh
@@ -2,22 +2,35 @@
 export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
 YAMLDIR=/opt/setup/k3s
 
-touch $YAMLDIR/config.log
+# Wait for the traefik ingressroute to be ready
+check_crd() {
+    kubectl get crd ingressroutes.traefik.containo.us &> /dev/null
+    return $?
+}
+
+while true; do
+    if check_crd; then
+        break
+    else
+        echo "Waiting for CRD"
+    fi
+    sleep 1
+done
 
 cd $YAMLDIR/ns
-kubectl apply -f . >> $YAMLDIR/config.log
+kubectl apply -f .
 
 cd $YAMLDIR/secret
-kubectl apply -f . >> $YAMLDIR/config.log
+kubectl apply -f .
 
 cd $YAMLDIR/configmap
-kubectl apply -f . >> $YAMLDIR/config.log
+kubectl apply -f .
 
 cd $YAMLDIR/service
-kubectl apply -f . >> $YAMLDIR/config.log
+kubectl apply -f .
 
 cd $YAMLDIR/deployment
-kubectl apply -f . >> $YAMLDIR/config.log
+kubectl apply -f .
 
 cd $YAMLDIR/ingress
-kubectl apply -f . >> $YAMLDIR/config.log
+kubectl apply -f .
