1c4fedd8deff2f6c6657bfcb5af33eb631606efa
Daniel Kofler <fox27374@gmail.com>
2025-09-24 20:00:53 +0200

    added ALB securoty group and rules

--- terraform/security.tf
+++ terraform/security.tf
@@ -16,86 +16,105 @@ resource "aws_security_group" "private" {
   }
 }
 
-resource "aws_vpc_security_group_ingress_rule" "ssh_from_external" {
-  security_group_id = aws_security_group.public.id
-
-  description = "SSH from external"
-  cidr_ipv4   = var.NW["external_v4_cidr"]
-  ip_protocol = "tcp"
-  to_port     = 22
-  from_port   = 0
+resource "aws_security_group" "alb" {
+  name        = "ALB"
+  description = "ALB"
+  vpc_id      = aws_vpc.devops-infra.id
   tags = {
-    Name = "ssh"
+    Name = "ALB"
   }
 }
 
-resource "aws_vpc_security_group_ingress_rule" "https_from_external" {
-  security_group_id = aws_security_group.public.id
+# INGRESS RULES
+# External to ALB HTTPS
+resource "aws_vpc_security_group_ingress_rule" "external-2-alb-https" {
+  security_group_id = aws_security_group.alb.id
 
-  description = "HTTPS from external"
+  description = "external-2-alb-https"
   cidr_ipv4   = var.NW["external_v4_cidr"]
   ip_protocol = "tcp"
   to_port     = 443
-  from_port   = 0
+  from_port   = 443
   tags = {
     Name = "https"
   }
 }
 
-resource "aws_vpc_security_group_egress_rule" "public_to_external" {
+# ALB to public subnet HTTP
+resource "aws_vpc_security_group_ingress_rule" "alb-2-public-http-in" {
   security_group_id = aws_security_group.public.id
 
-  description = "All to external"
-  cidr_ipv4   = var.NW["external_v4_cidr"]
-  ip_protocol = "-1"
+  description                  = "alb-2-public-http-in"
+  ip_protocol                  = "tcp"
+  to_port                      = 8080
+  from_port                    = 8080
+  referenced_security_group_id = aws_security_group.alb.id
   tags = {
-    Name = "internet"
+    Name = "http"
   }
 }
 
-resource "aws_vpc_security_group_ingress_rule" "ssh_from_public" {
-  security_group_id = aws_security_group.private.id
+# External to public subnet SSH
+resource "aws_vpc_security_group_ingress_rule" "external-2-public-ssh" {
+  security_group_id = aws_security_group.public.id
 
-  description = "SSH from public"
+  description = "external-2-public-ssh"
+  cidr_ipv4   = var.NW["external_v4_cidr"]
   ip_protocol = "tcp"
   to_port     = 22
-  from_port   = 0
+  from_port   = 22
+  tags = {
+    Name = "ssh"
+  }
+}
+
+# Public to private subnet SSH
+resource "aws_vpc_security_group_ingress_rule" "public-2-private-ssh" {
+  security_group_id = aws_security_group.private.id
+
+  description = "public-2-private-ssh"
+  ip_protocol = "tcp"
+  to_port     = 22
+  from_port   = 22
   cidr_ipv4   = var.NW["sn_public_cidr"]
   tags = {
     Name = "ssh"
   }
 }
 
-resource "aws_vpc_security_group_ingress_rule" "ssh_from_private" {
+# Public to private subnet HTTP
+resource "aws_vpc_security_group_ingress_rule" "public-2-private-http" {
   security_group_id = aws_security_group.private.id
 
-  description = "SSH from private"
-  ip_protocol = "tcp"
-  to_port     = 22
-  from_port   = 0
-  cidr_ipv4   = var.NW["sn_private_cidr"]
-  tags = {
-    Name = "ssh"
-  }
-}
-
-resource "aws_vpc_security_group_ingress_rule" "http_from_public" {
-  security_group_id = aws_security_group.private.id
-
-  description = "HTTP from public"
+  description = "public-2-private-http"
   ip_protocol = "tcp"
   to_port     = 80
-  from_port   = 0
+  from_port   = 80
   cidr_ipv4   = var.NW["sn_public_cidr"]
   tags = {
     Name = "http"
   }
 }
 
-resource "aws_vpc_security_group_ingress_rule" "icmp_from_private" {
+# Private to private subnet SSH
+resource "aws_vpc_security_group_ingress_rule" "private-2-private-ssh" {
   security_group_id = aws_security_group.private.id
 
-  description = "ICMP from private"
+  description = "private-2-private-ssh"
+  ip_protocol = "tcp"
+  to_port     = 22
+  from_port   = 22
+  cidr_ipv4   = var.NW["sn_private_cidr"]
+  tags = {
+    Name = "ssh"
+  }
+}
+
+# Public to private subnet ICMP
+resource "aws_vpc_security_group_ingress_rule" "public-2-private-icmp" {
+  security_group_id = aws_security_group.private.id
+
+  description = "public-2-private-icmp"
   ip_protocol = "icmp"
   from_port   = 8
   to_port     = 0
@@ -105,10 +124,66 @@ resource "aws_vpc_security_group_ingress_rule" "icmp_from_private" {
   }
 }
 
-resource "aws_vpc_security_group_egress_rule" "private_to_external" {
+# resource "aws_vpc_security_group_ingress_rule" "external-2-public-https" {
+#   security_group_id = aws_security_group.public.id
+
+#   description = "external-2-public-https"
+#   cidr_ipv4   = var.NW["external_v4_cidr"]
+#   ip_protocol = "tcp"
+#   to_port     = 443
+#   from_port   = 0
+#   tags = {
+#     Name = "https"
+#   }
+# }
+
+# EGRESS RULES
+# ALB to public HTTP
+resource "aws_vpc_security_group_egress_rule" "alb-2-public-http-out" {
+  security_group_id = aws_security_group.alb.id
+
+  description = "alb-2-public-http-out"
+  ip_protocol = "tcp"
+  cidr_ipv4   = var.NW["sn_public_cidr"]
+  to_port     = 8080
+  from_port   = 8080
+  tags = {
+    Name = "http"
+  }
+}
+
+# # ALB to private HTTP
+# resource "aws_vpc_security_group_egress_rule" "alb-2-private-http" {
+#   security_group_id = aws_security_group.alb.id
+
+#   description = "alb-2-private-http"
+#   ip_protocol                  = "tcp"
+#   cidr_ipv4   = var.NW["sn_private_cidr"]
+#   to_port                      = 80
+#   from_port                    = 80
+#   tags = {
+#     Name = "http"
+#   }
+# }
+
+
+# Public to external ALL
+resource "aws_vpc_security_group_egress_rule" "public-2-external-all" {
+  security_group_id = aws_security_group.public.id
+
+  description = "public-2-external-all"
+  cidr_ipv4   = var.NW["external_v4_cidr"]
+  ip_protocol = "-1"
+  tags = {
+    Name = "internet"
+  }
+}
+
+# Private to external ALL
+resource "aws_vpc_security_group_egress_rule" "private-2-external-all" {
   security_group_id = aws_security_group.private.id
 
-  description = "Private to external"
+  description = "private-2-external-all"
   cidr_ipv4   = var.NW["external_v4_cidr"]
   ip_protocol = "-1"
   tags = {
