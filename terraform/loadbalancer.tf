6769faae17004ce9beae07d740535b89c8f207e1
Daniel Kofler <fox27374@gmail.com>
2025-09-24 20:01:55 +0200

    changed guacamole target port to 8080

--- terraform/loadbalancer.tf
+++ terraform/loadbalancer.tf
@@ -2,7 +2,7 @@ resource "aws_lb" "devops-infra" {
   name               = "devops-infra"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.public.id]
+  security_groups    = [aws_security_group.alb.id]
   subnets            = [aws_subnet.public.id, aws_subnet.private.id]
 
   enable_deletion_protection = false
@@ -24,7 +24,7 @@ resource "aws_lb_listener" "devops-infra" {
     target_group_arn = aws_lb_target_group.guacamole.arn
   }
 
-  depends_on = [ aws_acm_certificate_validation.lab_validation ]
+  depends_on = [aws_acm_certificate_validation.lab_validation]
 }
 
 resource "aws_lb_target_group" "guacamole" {
@@ -32,12 +32,13 @@ resource "aws_lb_target_group" "guacamole" {
     interval            = 10
     path                = "/"
     protocol            = "HTTP"
+    port = 8080
     timeout             = 5
     healthy_threshold   = 5
     unhealthy_threshold = 2
   }
   name        = "guacamole"
-  port        = 80
+  port        = 8080
   protocol    = "HTTP"
   target_type = "instance"
   vpc_id      = aws_vpc.devops-infra.id
@@ -46,7 +47,7 @@ resource "aws_lb_target_group" "guacamole" {
 resource "aws_lb_target_group_attachment" "guacamole" {
   target_group_arn = aws_lb_target_group.guacamole.arn
   target_id        = aws_instance.bastion.id
-  port             = 80
+  port             = 8080
 }
 
 resource "aws_lb_target_group" "lab_tg" {
@@ -57,26 +58,26 @@ resource "aws_lb_target_group" "lab_tg" {
     timeout             = 5
     healthy_threshold   = 3
     unhealthy_threshold = 2
-    port = 80
+    port                = 80
   }
-  count        = var.EC2["lab_count"]
-  name         = aws_instance.lab[count.index].tags["Name"]
-  port         = 80
-  protocol     = "HTTP"
+  count       = var.EC2["lab_count"]
+  name        = aws_instance.lab[count.index].tags["Name"]
+  port        = 80
+  protocol    = "HTTP"
   vpc_id      = aws_vpc.devops-infra.id
-  target_type  = "instance"
+  target_type = "instance"
 
   stickiness {
-    enabled = true
-    type = "lb_cookie"
+    enabled         = true
+    type            = "lb_cookie"
     cookie_duration = 86400
   }
 }
 
 resource "aws_lb_target_group_attachment" "lab_attachment" {
-  count          = var.EC2["lab_count"]
-  target_group_arn  = aws_lb_target_group.lab_tg[count.index].arn
-  target_id         = aws_instance.lab[count.index].id
+  count            = var.EC2["lab_count"]
+  target_group_arn = aws_lb_target_group.lab_tg[count.index].arn
+  target_id        = aws_instance.lab[count.index].id
   port             = 80
 
   depends_on = [aws_lb_target_group.lab_tg]
@@ -102,9 +103,9 @@ resource "aws_lb_listener_rule" "guacamole_rule" {
 }
 
 resource "aws_lb_listener_rule" "lab_listener_rule" {
-  count          = var.EC2["lab_count"]
-  listener_arn   = aws_lb_listener.devops-infra.arn
-  priority       = count.index + 2 # Ensure unique priority for each rule
+  count        = var.EC2["lab_count"]
+  listener_arn = aws_lb_listener.devops-infra.arn
+  priority     = count.index + 2 # Ensure unique priority for each rule
 
   condition {
     host_header {
